dbacfb6c08ae30a4ec3a5775e81451a32e683240
tpp57 <tpptpptpp57@gmail.com>
2024-01-30 16:01:14 +0900

    slackのHTTP POSTに変更

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import firebase_admin
 from firebase_admin import db
 import time
 import requests
+import os
+import threading
+
+#slack HTTP POSTのURL
+#OSの環境変数にしたい
+url = "https://hooks.slack.com/services/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"
 
 # 監視するパスのリスト 
 paths = [
@@ -20,27 +26,40 @@ firebase_admin.initialize_app(options={
 # 各パスのprev_dataを格納する辞書
 prev_data = {path: None for path in paths}
 
-LINE_API_URL = 'https://notify-api.line.me/api/notify'
-LINE_API_TOKEN = os.environ['API_KEY']
+# Firebaseからデータを取得し、前回のデータと比較
+def check_data(path):
+    global prev_data
+    while True:
+        data = db.reference(path).get()
 
-#LINE送信関数
-def post_message(message):
-  headers = {'Authorization': 'Bearer ' + LINE_API_TOKEN}  
-  data = {'message': message}
-  response = requests.post(LINE_API_URL, headers=headers, data=data)
-  return response.status_code == 200
+        #初回の場合はデータを格納して終了
+        if data is None:
+            print(f'{path}: {data}')
+            prev_data[path] = data
 
-while True:
-  for path in paths:
-    ref = db.reference(path)
-    data = ref.get()
-    if data == prev_data[path]:
-      message = f"Error: Path {path} data has not changed"
-      print(message)
-	#失敗した場合LINEに送信
-      if not post_message(message):
-        print("Failed to send LINE notification")
-    else:
-      print(f"Path {path}: {data}") 
-      prev_data[path] = data
-  time.sleep(3600)
+        #正常更新の場合はデータを格納して終了
+        elif data != prev_data[path]:
+            print(f'{path}: {data}')
+            prev_data[path] = data
+
+        #更新がない場合slackに通知
+        elif data == prev_data[path]:
+            print(f'{path}: {data} 更新なし')
+            requests.post(url, data = json.dumps({
+                'text': f'{path}: {data}'
+            }))
+
+def main():
+    # スレッドを作成し、各パスを監視
+    threads = []
+    while True:
+        for path in paths:
+            t = threading.Thread(target=check_data, args=(path,))
+            t.start()
+            threads.append(t)
+
+        # 各スレッドの終了を待つ
+        for t in threads:
+            t.join()
+        
+        time.sleep(100)
