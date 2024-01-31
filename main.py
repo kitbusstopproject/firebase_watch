5a04aaeb2838bcc6ebbd3309542cf7ac55167f6d
tpp57 <tpptpptpp57@gmail.com>
2024-01-31 13:19:44 +0900

    スレッド修正

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import time
 import requests
 import os
 import threading
+import json
 
 #slack HTTP POSTのURL
 #OSの環境変数にしたい
-url = "URL"
+url = "https://dokoka_tekitouna_slack_url.com"
 
 # 監視するパスのリスト 
 paths = [
@@ -34,32 +35,42 @@ def check_data(path):
 
         #初回の場合はデータを格納して終了
         if data is None:
-            print(f'{path}: {data}')
+            print(f'{path}: {data} 初回')
             prev_data[path] = data
+            requests.post(url, data = json.dumps({
+                'text': f'{path}: {data} 初回'
+            }))
 
         #正常更新の場合はデータを格納して終了
         elif data != prev_data[path]:
-            print(f'{path}: {data}')
+            print(f'{path}: {data} 更新あり')
             prev_data[path] = data
+            requests.post(url, data = json.dumps({
+                'text': f'{path}: {data} 更新あり'
+            }))
 
         #更新がない場合slackに通知
         elif data == prev_data[path]:
             print(f'{path}: {data} 更新なし')
             requests.post(url, data = json.dumps({
-                'text': f'{path}: {data}'
+                'text': f'{path}: {data} 更新なし'
             }))
 
 def main():
-    # スレッドを作成し、各パスを監視
-    threads = []
-    while True:
-        for path in paths:
-            t = threading.Thread(target=check_data, args=(path,))
-            t.start()
-            threads.append(t)
 
-        # 各スレッドの終了を待つ
-        for t in threads:
-            t.join()
-        
-        time.sleep(100)
+  threads = []
+
+  while True:
+    print("while")
+    for path in paths:
+      print("for")  
+      t = threading.Thread(target=check_data, args=(path,))
+      threads.append(t)
+      t.start()
+    for t in threads:
+        t.join()
+        time.sleep(60)
+
+
+if __name__ == '__main__':
+  main()
